2f60fa6f369bc9c10f4f67ad0cc251c4c17dba1c
achrafhiro <151944398+achrafhiro@users.noreply.github.com>
2023-11-28 17:51:05 +0100

    first commit

---
+++ helloworld/tasks.json/helloworld.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code",};
+
+
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
